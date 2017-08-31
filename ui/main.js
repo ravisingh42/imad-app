1f8a5b010adb29982089741ec60ebecc4b39b9df
Ravi Singh <ravisingh78927@gmail.com>
2017-08-31 11:40:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,9 +9,9 @@ element.innerHTML = 'New value';
 var img= document.getElementById('madi');
 var marginLeft=0;
 function moveRight() {
-    marginLeft = marginLeft+1;
+    marginLeft = marginLeft+5;
     img.style.marginLeft = marginLeft +'px';
     }
 img.onclick= function() {
-    var interval = setInterval(moveRight, 50);
+    var interval = setInterval(moveRight, 100);
 };
