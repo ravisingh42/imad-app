2e112cd87fac8b1ffd819aa106d7de01e90463a0
Ravi Singh <ravisingh78927@gmail.com>
2017-09-01 00:49:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,8 +43,7 @@ button.onclick= function() {
 };
 
     //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
@@ -72,6 +71,8 @@ submit.onclick = function() {
     };
     
     // Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://ravisingh78927.imad.hasura-app.io/submit-name?name='+ name, true);
     request.send(null);
 };
