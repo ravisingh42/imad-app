9890bd87e518d81d10083dac4893f6f2a8eff18b
Ravi Singh <ravisingh78927@gmail.com>
2017-09-01 00:50:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,10 @@
 console.log('Loaded!');
+
+//Change the text of the main-text div
+var element = document.getElementById('main-text');
+
+element.innerHTML = 'New value';
+
 //move the image
 var img= document.getElementById('madi');
 var marginLeft=0;
