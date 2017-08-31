ee1fffa5a486850304443dc3001f2db808d416a7
Ravi Singh <ravisingh78927@gmail.com>
2017-08-31 21:14:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,16 @@ function moveRight() {
 img.onclick= function() {
     var interval = setInterval(moveRight, 10);
 };
+
+//Counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick= function() {
+    // Make a request to the counter end point
+    // Capture the responce and end it in the variable
+    // render the variable in the correct span
+    
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML= counter.toString();
+};
