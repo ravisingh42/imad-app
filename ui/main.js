f525fe774e97f8e6e2066f1e040158276feebe0a
Ravi Singh <ravisingh78927@gmail.com>
2017-08-31 23:06:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,4 +41,20 @@ button.onclick= function() {
     request.open('GET','http://ravisingh78927.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+//submit name
+var nameinput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    //Make a request to the server and send the name
     
+    //Capture a list as a name and render it to the list
+     var names= ['name1' , 'name2' , 'name3', 'name4'];
+     var list='';
+     for(i=0; i<names.lrngth ; i++) {
+         list +='<li>' + names[i] + '</li>';
+     }
+     var ul= document.getElementById('namelist');
+     ul.innerHTML = list;
+};
