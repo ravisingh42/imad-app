434f4d68096230d4480f56844737a040fa2f1834
Ravi Singh <ravisingh78927@gmail.com>
2017-09-01 00:44:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,32 +29,50 @@ button.onclick= function() {
         if (request.readyState=== XMLHttpRequest.DONE) {
             //Take some action
             if(request.status===200) {
-                var counter= request.responseText;
+                var counter = request.responseText;
                 var span = document.getElementById('count');
-                span.innerHTML=counter.toString();
+                span.innerHTML = counter.toString();
             }
         }
         //Not done yet
     };
-    
-    // Make the request
+        
+         // Make the request
     request.open('GET','http://ravisingh78927.imad.hasura-app.io/counter', true);
     request.send(null);
 };
 
-//submit name
+    //submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-    //Make a request to the server and send the name
     
-    //Capture a list as a name and render it to the list
-     var names= ['name1' , 'name2' , 'name3', 'name4'];
+    //Create a request object
+    var request = newXMLHttpRequest();
+    
+     // Capture the responce and change it into a variable
+    request.onreadystatechange = function () {
+        if (request.readyState=== XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status===200) {
+
+              //Capture a list as a name and render it to the list
+     var names= request.responseText;
+     names= JSON.parse(names);
      var list='';
      for(i=0; i<names.length ; i++) {
          list +='<li>' + names[i] + '</li>';
      }
      var ul= document.getElementById('namelist');
      ul.innerHTML = list;
+            }
+        }
+        //Not done yet
+    };
+    
+    // Make the request
+    request.open('GET','http://ravisingh78927.imad.hasura-app.io/submit-name?name='+ name, true);
+    request.send(null);
 };
+
