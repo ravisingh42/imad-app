5fe948522a037f21242f014ec9cc3958ba7f1254
Ravi Singh <ravisingh78927@gmail.com>
2017-08-31 20:43:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,13 +99,11 @@ app.get('/', function (req, res) {
 });
 
 var counter = 0;
-app.get('/counter',function(req,res){
+app.get('/counter',function (req, res){
     counter = counter + 1;
     res.send(counter.toString());
 });
     
-})
-
 app.get('/:articleName',function(req,res){
     //articleNae == article-one
     // articles[articleName] == {} content object for article one
