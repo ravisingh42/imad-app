531ad8e7a6297b0c9502d47060ac172f7a1411bb
Ravi Singh <ravisingh78927@gmail.com>
2017-09-03 15:48:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,18 +107,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new pool(config);
-app.get('/test-db', function(req, res) {
+//var pool = new pool(config);
+//app.get('/test-db', function(req, res) {
     //Make a select request
     //Return with response with the results
-    pool.query('SELECT * FROM test', function(err, result) {
-        if(err) {
-            res.status(500).send(err.toString());
-            } else {
-                res.send(JSON.stringify(result));
-            }
-    });
-});
+  //  pool.query('SELECT * FROM test', function(err, result) {
+       // if(err) {
+      //      res.status(500).send(err.toString());
+        //    } else {
+           //     res.send(JSON.stringify(result));
+  //          }
+  //  });
+//});
 
 
 var counter = 0;
