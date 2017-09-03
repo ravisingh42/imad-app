25cd06db3146938819790a92eefeb963406b984d
Ravi Singh <ravisingh78927@gmail.com>
2017-09-03 15:45:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').pool;
+var pool = require('pg');
 
 var config = {
     user: 'ravisingh78927',
