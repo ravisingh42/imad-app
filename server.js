96e1392b945a40124568dda3c6679e0c436f1539
Ravi Singh <ravisingh78927@gmail.com>
2017-09-03 14:24:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+
+var config = {
+    user: 'ravisingh78927',
+    database: 'ravisingh78927',
+    host: 'dp.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    };
 
 var app = express();
 app.use(morgan('combined'));
@@ -97,6 +106,20 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new pool(config);
+app.get('/test-db', function(req, res) {
+    //Make a select request
+    //Return with response with the results
+    pool.query('SELECT * from test', function(err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
+            } else {
+                res.send(JSON.stringify(result));
+            }
+    });
+});
+
 
 var counter = 0;
 app.get('/counter',function (req, res){
